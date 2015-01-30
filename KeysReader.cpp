a56642b23f14f9eebb413dcaa86dfa1b23f81ea0
GermanRGomez <grgomez@ualberta.ca>
2015-01-29 20:50:32 -0700

    Added KeysReader class. It will determine whether keys are being pressed and the commands that they represent.

---
+++ KeysReader.cpp
@@ -0,0 +1,13 @@
+#include "KeysReader.hpp"
+#include <conio.h>
+
+KeysReader::KeysReader()
+{
+	// To be determined
+}
+
+KeysReader::~KeysReader()
+{
+	// To be determined
+}
+
---
+++ KeysReader.hpp
@@ -0,0 +1,12 @@
+#ifndef KEYSREADER_HPP
+#define KEYSREADER_HPP
+
+class KeysReader
+{
+public:
+	KeysReader();
+	~KeysReader();
+
+};
+
+#endif // KEYSREADER_HPP
